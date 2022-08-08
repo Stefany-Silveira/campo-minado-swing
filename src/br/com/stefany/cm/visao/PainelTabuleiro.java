55aa6506d5aa2b6c30e42fc048aec97f180bbaf4
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-08 19:42:34 -0300

    exemplo @ Interface Gráfica do Campo Minado #01

---
+++ src/br/com/stefany/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,15 @@
+package br.com.stefany.cm.visao;
+
+import br.com.stefany.cm.modelo.Tabuleiro;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+    }
+
+
+}
