f9bd707f53315cb60ab1fc54c5f60c807c5a2330
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-09 10:42:46 -0300

    exemplo @ Interface Gráfica do Campo Minado #02

---
+++ src/br/com/stefany/cm/visao/BotaoCampo.java
@@ -0,0 +1,14 @@
+package br.com.stefany.cm.visao;
+
+import br.com.stefany.cm.modelo.Campo;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton {
+
+    public BotaoCampo(Campo campo) {
+
+    }
+
+}
